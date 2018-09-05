9e1f1f9e3a2c34c24fcec276e0e29b16731969b8
Valtteri Salmijärvi <38540196+wazba@users.noreply.github.com>
2018-09-05 18:52:40 +0300

    Update MainRot.py

--- MainRot.py
+++ MainRot.py
@@ -4,10 +4,11 @@ ABC = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
        'N', 'O', 'P', 'Q', 'R', 'S','T','U','V','W','X','Y','Z']
 rot = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 rotY = [0]
-Text = ['Text is: ']
+Text = ['This will be overwritten ']
 def rotX(Rot, text):
     Check = 0
     Text.clear()
+    rotY.clear()
     new = list(text)
     rotY[0] = Rot
     for i in range(1, 26):
